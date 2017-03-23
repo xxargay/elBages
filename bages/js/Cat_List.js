a1660357a9ef8a23bffcc49428dbabaa5802d0ef
XAVI <xxargay@yahoo.com>
2017-03-23 18:02:06 +0100

    inicial

---
+++ bages/js/Cat_List.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+
+				
+	ListCats = [	["001", "Restaurants"],
+					["002", "Hotels"],
+					["003", "Productes típics"],
+					["004", "Aparcaments"],
+					["005", "Shopping"],
+					["006", "Punts d'informació"],
+					["007", "Telèfons d'interès"]
+				]
